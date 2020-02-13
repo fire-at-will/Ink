3f36c61e8ff9484f4ef471f672d395b63edc75d3
Will Taylor <wtaylor151@gmail.com>
2020-02-13 15:40:28 -0600

    Add version info to package file

--- Package.swift
+++ Package.swift
@@ -10,6 +10,10 @@ import PackageDescription
 
 let package = Package(
     name: "Ink",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(SupportedPlatform.IOSVersion.v13),
+    ]
     products: [
         .library(name: "Ink", targets: ["Ink"]),
         .executable(name: "ink-cli", targets: ["InkCLI"])
@@ -19,4 +23,5 @@ let package = Package(
         .target(name: "InkCLI", dependencies: ["Ink"]),
         .testTarget(name: "InkTests", dependencies: ["Ink"])
     ]
+    
 )
